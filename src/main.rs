d4821d356b94d3bbfbe77253bd1f749852066aca
= <=>
2026-06-20 01:29:05 -0600

    Implementacion del sistema de tests

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,68 @@
 use axum::{
-    routing::get,
-    Router
+    Router,
+extract::ConnectInfo,
+    routing::{get, post},
+    Json,
 };
-use diesel::prelude::*;
-use dotenvy::dotenv;
-use std::env;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 pub mod handler;
-pub mod schema;
 pub mod state;
 
 #[tokio::main]
 async fn main() {
-    let atmos_entries = Router::new().route("/prueba", get( || async {"hola 1"}) );
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    let api_v01 = Router::new().nest("/v01", atmos_entries);
+   
+    let app = app();
 
-    let app = Router::new().merge(api_v01).fallback(get(|| async {"ruta no valida"}));
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.expect("No se inicializar al puerto 3000");
+    axum::serve(listener, app).await.expect("No se pudo vincular el listener a la app");
 
 }
 
+fn app() -> Router {
+    let atmos_entries = Router::new().route("/prueba", get( || async {"hola 1"}) );
+    let api_v01 = Router::new().nest("/v01", atmos_entries);
+    Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .merge(api_v01).fallback(get(|| async {"ruta no valida"}))
+        // We can still add middleware
+        //.layer(TraceLayer::new_for_http())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+     use axum::{
+        body::Body,
+        extract::connect_info::MockConnectInfo,
+        http::{self, Request, StatusCode},
+    };
+    use http_body_util::BodyExt; // for `collect`
+    use serde_json::{json, Value};
+    use tokio::net::TcpListener;
+    use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready`
+    
+    #[tokio::test]
+    async fn get_hello_world() {
+        let app = app();
+        let response = app
+            .oneshot(Request::get("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+          assert_eq!(response.status(), StatusCode::OK);
+          let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(&body[..], b"Hello, World!");
+    }
+    
+}
--- src/state.rs
+++ src/state.rs
@@ -1,81 +1,31 @@
 use std::sync::{Arc, RwLock};
 use chrono::Local;
-use dotenvy::dotenv;
-use std::env;
-use diesel::prelude::*;
-use diesel::r2d2::ConnectionManager;
-use diesel::r2d2::Pool;
-use diesel::result::Error;
+use serde::{Deserialize, Serialize};
 
-
-
-#[derive(Clone)]
-struct LastRead{
-    hour : Arc<RwLock<String>>, 
-    date : Arc<RwLock<String>>,
-    timestamp : Arc<RwLock<String>>
-}
-
-#[derive(Clone)]
-struct BatteryStatus{
-    amps_h : Arc<RwLock<u32>>    
-}
-
-#[derive(Clone)]
-struct SqlitePool{
-    //pool : Pool<ConnectionManager<DbConnection>>
-}
-
-// impl SqliteConn {
-//     pub fn from(val: Arc<RwLock<SqliteConnection>>) -> Self {
-//         Self { conn:  }
-//     }
-// }
-impl BatteryStatus {
-    pub fn from(val : u32) -> Self{
-        Self { amps_h: Arc::new(RwLock::new(val))}
-    }
-    pub fn from_fixed(val: Arc<RwLock<u32>>) -> Self {
-        Self { amps_h: val }
-    }
-}
-impl LastRead {
-    pub fn new() -> Self {
-        let now = Local::now();
-        let date_f = Arc::new(RwLock::new(now.format("%Y-%m-%d").to_string()));
-        let hour_f = Arc::new(RwLock::new(now.format("%H/%M/%S").to_string()));
-        let timestamp_f = Arc::new(RwLock::new(now.format("%Y-%m-%d %H/%M/%S").to_string()));
-        Self { 
-            date : date_f,
-            hour : hour_f,
-            timestamp : timestamp_f
-        }
-    }
-}
-
-#[derive(Clone)]    
+#[derive(Clone, Serialize, Deserialize)]    
 struct AppState{
-    pub battery_status : BatteryStatus,
-    pub last_read : LastRead,
-    //pub sqlite_conn : SqliteConn,
+    pub date : String,
+    pub hour : String, 
+    pub battery : u32,
+    pub sleep : bool,
+
 }
 
 impl AppState {
-    fn new() -> Self {
-        dotenv().ok();
-        
-        // ruta de la db dentro de .env 
-        let database_url = env::var("DATABASE_URL").expect("Falta la variable DATABASE_URL");
-        // conexion a db
-        let sqlite_conn = SqliteConnection::establish(&database_url)
-            .unwrap_or_else(|_| panic!("Error al conectarse a {}", database_url));
-        // valor encapsulado 
-        let fixed_conn = Arc::new(RwLock::new(sqlite_conn));
-        
-        Self{
-            battery_status : BatteryStatus::from(0),
-            last_read : LastRead::new(),
-            //sqlite_conn : SqliteConn::from(fixed_conn)
-        }
+    pub fn new() -> Arc<RwLock<AppState>> {
+        let now = Local::now();
+        Arc::new(RwLock::new(Self{
+            date : now.format("%Y/%m/%d").to_string(),
+            hour : now.format("%H:%M:%S").to_string(),
+            battery : 0,
+            sleep : false
+        }))
+    }
+    pub fn update(fixed_state : &mut Arc<RwLock<AppState>>, new_state : AppState ) {
+        let mut writter_guard = fixed_state.write().unwrap();
+        *writter_guard = new_state;
     }
 }
+
+
+
